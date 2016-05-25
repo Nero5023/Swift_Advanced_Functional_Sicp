698ba2bd69e450abaaa9a769442f1df8291e84c3
Nero5023 <502350123@qq.com>
2016-05-26 00:47:30 +0800

    Finish daliy functional swift reading

---
+++ FunctionalSwift/3_Core_image.playground/Contents.swift
@@ -0,0 +1,21 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+typealias Filter = CIImage -> CIImage
+
+
+func blur(radius: Double) -> Filter {
+  return { image in
+    let parameters = [
+      kCIInputRadiusKey: radius,
+      kCIInputImageKey: image
+    ]
+    guard let filter = CIFilter(name: "CIGaussianBlur", withInputParameters: parameters) else
+      { fatalError() }
+    guard let outputImage = filter.outputImage else { fatalError() }
+    return outputImage
+  }
+}

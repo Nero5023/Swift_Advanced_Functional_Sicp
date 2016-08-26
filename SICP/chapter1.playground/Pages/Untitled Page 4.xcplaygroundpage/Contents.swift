c032bba58a2a72229ad957faf95fded2826d199c
Nero Zuo <502350123@qq.com>
2016-08-26 23:08:13 +0800

    no message

--- SICP/chapter1.playground/Pages/Untitled Page 4.xcplaygroundpage/Contents.swift
+++ SICP/chapter1.playground/Pages/Untitled Page 4.xcplaygroundpage/Contents.swift
@@ -2,6 +2,8 @@
 
 import Foundation
 
+//: # 1.3 用高阶函数做抽象
+
 // 函数的解 区间折半法
 func search(negPoint negPoint: Double, posPoint: Double, function: (Double)->Double) -> Double {
   let midPoint = (negPoint + posPoint) / 2.0
@@ -79,11 +81,14 @@ func sqrtNew(x: Double) -> Double {
 }
 //sqrtNew(2)
 
+
+//: ## 1.35
 // 黄金比例数
 //fixedPoint(firstguess: 2.0) { x in
 //  1 + 1/x
 //}
 
+//: ## 1.36
 //非平均阻尼
 fixedPoint(firstguess: 2) { x in
   log(1000)/log(x)
@@ -92,3 +97,65 @@ fixedPoint(firstguess: 2) { x in
 fixedPoint(firstguess: 2) { x in
   (log(1000)/log(x) + x)/2
 }
+
+
+//: ## 1.37
+// 递归版
+func contFrac(k: Int, nFunc: (Int)->Double, dFunc: (Int)->Double) -> Double {
+//  nFunc(k) / (dFunc(k)  )
+  func cf(i: Int) -> Double {
+    if i==k {
+      return nFunc(i) / dFunc(i)
+    }else {
+      return nFunc(i) / (dFunc(i)+cf(i+1))
+    }
+  }
+  
+  return cf(1)
+}
+
+contFrac(10, nFunc: { _ in 1 }, dFunc: { _ in 1 })
+
+// 迭代版 考虑先算什么再算什么
+func contFracIteration(k: Int, nFunc: (Int)->Double, dFunc: (Int)->Double) -> Double {
+  func iter(i i: Int, result: Double) -> Double {
+    if i == 0 {
+      return result
+    }
+    return iter(i: i-1, result: nFunc(i) / (dFunc(i) + result))
+  }
+  return iter(i: k, result: 0)
+}
+
+contFracIteration(11, nFunc: { _ in 1 }, dFunc: { _ in 1 })
+
+// e - 2 = 
+// contFrac 上面的这个过程 nfunc = { _ in 1 } , dfunc = 1, 2, 1, 1, 4, 1, 1, 6, 1, 1, 8
+
+func calculate_e(k: Int) -> Double {
+  func dfunc(k: Int) -> Double {
+    let i = k/3 + 1
+    return k % 3 == 2 ? Double(i*2) : Double(1)
+  }
+  return contFrac(k, nFunc: { _ in 1 }, dFunc: dfunc) + 2
+}
+calculate_e(11)
+
+// 正切函数 连分式
+func tan_cf(x x: Double, k: Int) -> Double {
+  func dfunc(i: Int) -> Double {
+    return Double(2*i - 1)
+  }
+  func nfunc(i: Int) -> Double {
+    return i == 1 ? x : -x*x
+  }
+  return contFracIteration(k, nFunc: nfunc, dFunc: dfunc)
+}
+
+tan_cf(x: 10, k: 100)
+tan(10.0)
+tan_cf(x: 25, k: 100)
+tan(25.0)
+
+
+//: [Next](@next)

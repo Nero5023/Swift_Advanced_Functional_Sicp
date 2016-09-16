be642e5697af40d5c57922c93522677b1c6dcae1
Nero Zuo <502350123@qq.com>
2016-09-17 00:21:24 +0800

    75-make-from-mag-ang

---
+++ SICP/Scheme/chapter2/75-make-from-mag-ang.scm
@@ -0,0 +1,14 @@
+(define (make-from-mag-ang x y)
+    (define (dispatch op)
+        (cond ((eq? op 'real-part)
+                (* x (cos y)))
+              ((eq? op 'imag-part)
+                (* x (sin y)))
+              ((eq? op 'magnitude)
+                x)
+              ((eq? op 'angle)
+                y)
+              (else
+                (error "Unkonw op  -- MAKE-FROM-MAG-ANG" op))))
+    dispatch
+)

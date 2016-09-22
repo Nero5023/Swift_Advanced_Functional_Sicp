6b1bff14883e0b858320520c85f6084f9e7b9d75
Nero Zuo <502350123@qq.com>
2016-09-22 11:56:44 +0800

    make-account

---
+++ SICP/Scheme/chapter3/3-make-account2.scm
@@ -0,0 +1,25 @@
+(define (make-account balance password)
+    (define (withdraw amount)
+        (if (>= balance amount)
+            (begin (set! balance (- balance amount))
+                balance)
+            "Insuficient amount")
+    )
+
+    (define (deposit amount)
+        (set! balance (+ balance amount))
+        balance
+    )
+
+    (define (dispatch password-input m)
+        (if (eq? password-input password)
+                (cond ((eq? m 'withdraw) withdraw)
+                      ((eq? m 'deposit) deposit)
+                      (else 
+                            (error "Unknow request --MAKE-ACCOUNT"))
+                )
+            (lambda (x) (display "Incroect password"))
+        )
+    )
+    dispatch
+)

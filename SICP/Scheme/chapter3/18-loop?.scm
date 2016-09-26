37d1a42e46773c4e52639207b2aa8316d1e72fbd
Nero Zuo <502350123@qq.com>
2016-09-26 20:47:14 +0800

    18-loop?

---
+++ SICP/Scheme/chapter3/18-loop?.scm
@@ -0,0 +1,13 @@
+(define (loop? lst)
+    (let ((identity (cons '() '())))
+        (define (iter remain-list)
+            (cond ((null? remain-list) #f)
+                    ((eq? identity (car remain-list)) #t)        
+                    (else 
+                        (set-car! remain-list identity)
+                        (iter (cdr remain-list))
+                    )
+            )  
+        )
+    (iter lst))
+)

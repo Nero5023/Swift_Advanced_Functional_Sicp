d9b9e5d656f577dac32be3428caf12ce6d0fbbae
Nero Zuo <502350123@qq.com>
2016-10-06 01:36:36 +0800

    or-gate

---
+++ SICP/Scheme/chapter3/28-or-gate.scm
@@ -0,0 +1,34 @@
+(define (logical-not s)
+    (cond ((= s 0) 
+            1)
+          ((= s 1) 0)
+          (else 
+            (error "Incalid signal" s)))
+)
+
+(define (and-gate a1 a2 output)
+    (define (and-action-procedure)
+        (let ((new-value (logical-and (get-signal a1) (get-signal a2))))
+            (after-daly and-gate-delay
+                (lambda () (set-signal! output new-value))))
+    )
+
+    (add-action! a1 and-action-procedure)
+    (add-action! a2 and-action-procedure)
+)
+
+(define (logical-or a1 a2)
+    (if (or (= a1 1) (= a2 1))
+        1
+        0))
+
+(define (or-gate a1 a2 output)
+    (define (or-action-procedure)
+        (let ((new-value (logical-or (get-signal a1) (get-signal a2))))
+            (after-daly or-gate-delay
+                (lambda () (set-signal! output new-value))))
+    )
+
+    (add-action! a1 or-action-procedure)
+    (add-action! a2 or-action-procedure)
+)

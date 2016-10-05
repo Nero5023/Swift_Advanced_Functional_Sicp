bed4076bb81f87f77abd75fd35792a05bfba76d3
Nero Zuo <502350123@qq.com>
2016-10-06 01:31:56 +0800

    page-191

---
+++ SICP/Scheme/chapter3/page-191.scm
@@ -0,0 +1,18 @@
+(define (logical-not s)
+    (cond ((= s 0) 
+            1)
+          ((= s 1) 0)
+          (else 
+            (error "Incalid signal" s)))
+)
+
+(define (and-gate a1 a2 output)
+    (define (and-action-procedure)
+        (let ((new-value (logical-and (get-signal a1) (get-signal a2))))
+            (after-daly and-gate-delay
+                (lambda () (set-signal! output new-value))))
+    )
+
+    (add-action! a1 and-action-procedure)
+    (add-action! a2 and-action-procedure)
+)

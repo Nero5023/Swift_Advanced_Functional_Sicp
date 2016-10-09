c60db8ecf7dfef9845020abf5b28833a7cf796be
Nero Zuo <502350123@qq.com>
2016-10-09 23:30:27 +0800

    33-averager

---
+++ SICP/Scheme/chapter3/33-averager.scm
@@ -0,0 +1,8 @@
+(define (averager a b c)
+    (let ((sum (make-connector))
+          (d (make-connector)))
+        (adder a b sum)
+        (multiplier sum d c)
+        (constant (/ 1 2) d)
+        'ok)
+)

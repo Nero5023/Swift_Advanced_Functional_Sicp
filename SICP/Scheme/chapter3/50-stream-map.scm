2ee63bde7f3e1811fb2585499d1c391f9c7c130e
Nero Zuo <502350123@qq.com>
2016-10-23 21:02:52 +0800

    no message

---
+++ SICP/Scheme/chapter3/50-stream-map.scm
@@ -0,0 +1,8 @@
+(define (stream-map proc . list-of-stream)
+    (if (null? (car list-of-stream))
+        '()
+        (cons-stream (apply proc (map (lambda (s) (stream-car s)) list-of-stream))
+            (apply stream-map (cons proc (map (lambda (s) (stream-cdr s)) list-of-stream)))
+        )    
+    )
+)

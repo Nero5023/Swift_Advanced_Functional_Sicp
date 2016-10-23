c46bb3a449d9f7f93ed815ec822185383ac901e7
Nero Zuo <502350123@qq.com>
2016-10-23 23:32:48 +0800

    52-lazy2

---
+++ SICP/Scheme/chapter3/52-lazy2.scm
@@ -0,0 +1,12 @@
+(define sum 0)
+
+(define (accum x)
+  (set! sum (+ sum x))
+sum)
+
+(define seq (stream-map accum (stream-enumerate-interval 1 20)))
+(define y (stream-filter even? seq))
+(define z (stream-filter (lambda (x) (= (remainder x 5) 0)) seq))
+
+(stream-ref y 7)
+(display-stream z)

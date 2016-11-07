e416450eb6d0eb447d5133548eb665b6bec162a9
Nero Zuo <502350123@qq.com>
2016-11-07 23:15:43 +0800

    63-sqrt-sream

---
+++ SICP/Scheme/chapter3/63-sqrt-stream.scm
@@ -0,0 +1,7 @@
+(define (sqrt-stream x)
+    (define guesses
+        (cons-stream 1.0
+                     (stream-map (lambda (guess)
+                                     (sqrt-improve guess x))
+                                 guesses)))
+    guesses)

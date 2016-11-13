22698f4e2b7449eb0a3190e5d33fe4d3be16ebd6
Nero Zuo <502350123@qq.com>
2016-11-13 15:27:35 +0800

    4-eval-and

---
+++ SICP/Scheme/chapter4/4-eval-and-or.scm
@@ -0,0 +1,34 @@
+(define (tagged-list? exp tag)
+  (if (pair? exp)
+      (eq? (car exp) tag)
+      #f))
+
+
+(define (and? exp)
+  (tagged-list? exp 'and))
+
+(define (or? exp)
+  (tagged-list? exp 'or))
+
+(define (logic-predicates exp)
+  (cdr exp))
+
+(define (first-predicate predicates)
+  (car predicates))
+
+(define (rest-predicates predicates)
+  (cdr predicates))
+
+(define (last-predicate? predicates)
+  (null? (cdr predicates)))
+
+(define (eval-and predicates env)
+    (cond ((null? predicates) #t)
+          ((last-predicate? predicates) (true? (first-predicate predicates) env))
+          ((true? (first-predicate predicates) env)
+                (eval-and (rest-predicates predicates) env))
+          (else
+            #f)
+    )
+)
+

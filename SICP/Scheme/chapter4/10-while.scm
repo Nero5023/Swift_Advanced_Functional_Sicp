f31197a34d7e5d9592bfcf5f009a7df4245ab0d2
Nero Zuo <502350123@qq.com>
2016-11-14 23:10:39 +0800

    10-while

---
+++ SICP/Scheme/chapter4/10-while.scm
@@ -0,0 +1,22 @@
+(define (tagged-list? exp tag)
+  (if (pair? exp)
+      (eq? (car exp) tag)
+      #f))
+
+(define (while? exp)
+  (tagged-list? exp 'while))
+
+(define (while-condition exp)
+  (cadr exp))
+
+(define (while-body exp)
+  (caddr exp))
+
+
+(define (while->combination exp)
+    (sequence->exp (list (list 'define (list 'while-iter')
+        (make-if (while-condition exp) (sequence->exp (list (while-body exp)
+            (list 'while-iter))) 'true)) (list 'while-iter)))
+)
+
+

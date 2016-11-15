750c62a5a66e6d34be1e11050284fca178cbd808
Nero Zuo <502350123@qq.com>
2016-11-15 22:32:36 +0800

    13-make-unbound

--- SICP/Scheme/chapter4/13-make-unbound.scm
+++ SICP/Scheme/chapter4/13-make-unbound.scm
@@ -3,11 +3,21 @@
       (eq? (car exp) tag)
       #f))
 
-(define (make-unbound? exp)
-  (tagged-list exp 'make-unbound!))
-
-(define (eval-make-bound exp env)
-    (set-variable-value! (cadr exp) '() env)
+; 基于书上原来方式实现的frame
+(define (make-bound! var env)
+    (let (frame (first-frame env)
+          (vars (frame-variables frame))
+          (vals (frame-values frame)))
+        (define (scan pre-vars pre-vals vars vals)
+            (if (not (null? vars))
+                (if (eq? (car vars) var)
+                    (begin (set-cdr! pre-vars (cdr vars))
+                            (set-cdr! pre-vals (cdr vals)))
+                    (scan vars vals (cdr vars) (cdr vals)))
+                )
+          )
+        (scan '() '() vars vals)
+    )
 )
 
 (define (make-unbound-exp var)

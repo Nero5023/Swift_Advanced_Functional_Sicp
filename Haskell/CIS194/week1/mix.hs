3a985fdc5c2672a70d09175efc94aff302c95df0
Nero Zuo <502350123@qq.com>
2016-12-03 22:36:18 +0800

    week1

--- Haskell/CIS194/week1/mix.hs
+++ Haskell/CIS194/week1/mix.hs
@@ -1,3 +1,17 @@
 intListLength :: [Integer] -> Integer
 intListLength []     = 0
 intListLength (x:xs) = 1 + intListLength xs
+
+
+hailstoneSeq :: Integer -> [Integer]
+hailstoneSeq 1 = [1]
+hailstoneSeq n = n : hailstoneSeq (hailstone n)
+
+
+hailstone :: Integer -> Integer
+hailstone n
+  | n `mod` 2 == 0 = n `div` 2
+  | otherwise      = 3*n + 1
+
+hailstoneLen :: Integer -> Integer
+hailstoneLen n = intListLength (hailstoneSeq n) - 1

b7c9773e3021d3477e699a8c1997f87f91a7a00a
Nero Zuo <502350123@qq.com>
2016-12-03 22:36:38 +0800

    haskell week4

---
+++ Haskell/chap4/recursionFuncs.hs
@@ -0,0 +1,27 @@
+zip' :: [a] -> [b] -> [(a, b)]
+zip' _ [] = []
+zip' [] _ = []
+zip' (x:xs) (y:ys) = (x, y) : zip' xs ys
+
+
+
+replicate' :: Int -> a -> [a]
+replicate' n x
+    | n <= 0    = []
+    | otherwise = x : replicate' (n-1) x
+
+take' :: (Num i, Ord i) => i -> [a] -> [a]
+take' n _
+    | n <=0 = []
+take' _ [] = []
+take' n (x:xs) = x:take' (n-1) xs
+
+reverse' :: [a] -> [a]
+reverse' [] = []
+reverse' (x:xs) = reverse' xs ++ [x]
+
+elem' :: (Eq a) => a -> [a] -> Bool
+elem' _ [] = False
+elem' a (x:xs)
+    | a == x = True
+    | otherwise = a `elem'` xs

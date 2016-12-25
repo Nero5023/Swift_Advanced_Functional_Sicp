bf0acf11dfe393cfc339fe023f3b1baf13513f4d
Nero Zuo <502350123@qq.com>
2016-12-26 00:45:41 +0800

    exercise 2

--- Haskell/CIS194/week5/Calc.hs
+++ Haskell/CIS194/week5/Calc.hs
@@ -1,8 +1,18 @@
 module Calc where
 
 import ExprT
+import Parser
 
+-- Exercise 1
 eval :: ExprT -> Integer
 eval (Lit int) = int
 eval (Add lhs rhs) = (eval lhs) + (eval rhs)
 eval (Mul lhs rhs) = (eval lhs) * (eval rhs)
+
+
+-- Exercise 2
+evalStr :: String -> Maybe Integer
+evalStr exp = case parseExp Lit Add Mul exp of
+                        (Just exprt) -> Just $ eval exprt
+                        Nothing -> Nothing
+

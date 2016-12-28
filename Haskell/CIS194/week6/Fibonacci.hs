9fe21a62ef281b8a8284321bff14895f261103ab
Nero Zuo <502350123@qq.com>
2016-12-28 21:44:45 +0800

    ererisice2

--- Haskell/CIS194/week6/Fibonacci.hs
+++ Haskell/CIS194/week6/Fibonacci.hs
@@ -4,6 +4,15 @@ fib 0 = 0
 fib 1 = 1
 fib n = fib (n-1) + fib (n-2)
 
-
 fibs1 :: [Integer]
 fibs1 = [x | i <- [0,1..], let x = fib i ]
+
+-- Exercise 2
+fibs2 :: [Integer]
+fibs2 = [0,1] ++ [x | i<-[2..], let x = fibs2!!(i-1) + fibs2!!(i-2)]
+
+fibs2' :: [Integer]
+fibs2' = 0:1:zipWith (+) fibs2' (tail fibs2')
+
+-- Exercise 3
+data Stream a = Cons a (Stream a)
